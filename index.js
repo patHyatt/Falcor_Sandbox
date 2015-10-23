83f1e853f89d63f8742a58704d5f34062398e19a
Patrick Hyatt <patrick.hyatt@cision.com>
2015-10-22 23:16:08 -0700

    Add request to pull random json for merging. Add homemade route and consumer to try and figure out how this guy works.

--- index.js
+++ index.js
@@ -5,29 +5,44 @@ var Router = require('falcor-router');
 var express = require('express');
 var app = express();
 
-app.use('/model.json', falcorExpress.dataSourceRoute(function(req, res) {
-    // create a Virtual JSON resource with single key ("greeting")
-    return new Router([{
-        // match a request for the key "greeting"
-        route: "alpha",
-        // respond with a PathValue with the value of "Hello World."
-        get: function() {
-            return {
-                path: ["alpha"],
-                value: "Hellos World"
-            };
-        }
-    }]);
-}));
+var request = require("request");
 
 app.use('/crazy.json', falcorExpress.dataSourceRoute(function(req, res) {
     return new Router([{
-        route: "mix",
-        get: function() {
-            return {
-              path: ["love"],
-              value: "hate"
+        route: "tagsByIndex[{integers:index}]",
+        get: function(pathSet) {
+
+        	//Does not work..
+            // request(
+            //     'https://gist.githubusercontent.com/patHyatt/6b6988c91627afdd5187/raw/cb973382c3fbc624b739b1ce278d76fa4eb7c4fe/random.json',
+            //     function(error, response, body) {
+            //         console.error(error);
+            //         var json = JSON.parse(body);
+
+            //         var result = {
+            //             path: ['tagsByIndex'],
+            //             value: json.tags
+            //         };
+            //         console.log(result);
+            //         return result;
+            //     });
+
+    		//Does work...
+            var nonRequestedJson = {
+                "id": 1,
+                "tags": [
+                    "lame",
+                    "maybe",
+                    "no"
+                ]
             };
+            var result = {
+                path: ['tagsByIndex'],
+                value: nonRequestedJson.tags
+            };
+            console.log(result);
+            return result;
+
         }
     }]);
 }));
